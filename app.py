6c691005f44b1ec44536e8302122cab6cf479405
Adrian Gould (Lec) <adrian.gould@nmtafe.wa.edu.au>
2024-09-11 17:14:53 +0800

    init: Set Up Project

---
+++ app.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(title="FastAPI Templating")
+
+BASE_PATH = Path(__file__).resolve().parent
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+TEMPLATES = Jinja2Templates(directory="templates")
+
+print(BASE_PATH)
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return TEMPLATES.TemplateResponse(
+        request=request, name="home.html"
+    )
+
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "static/**/*.{js,html,py}",
+      "templates/**/*.{js,html,py}",
+      "app.py"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
