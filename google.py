a0c8a295426fdebf68a76b98fc1844499bc5080a
Ady G Code <ady.g.coder@gmail.com>
2024-09-30 07:23:42 +0800

    feat(google): Add blank Google OAuth

---
+++ google.py
@@ -0,0 +1,10 @@
+# -------------------------------------------------------------------
+# Project:    InterRIoT-2024S2-FastAPI-Security-2
+# Filename:   google.py
+# Location:   ./
+# Author:     Adrian Gould <adrian.gould@nmtafe.wa.edu.au>
+# Created:    2024-09-29
+# Purpose:
+#    Providing Google OAuth2 authentication code
+#    
+# ---------------------------------------------------------------------
